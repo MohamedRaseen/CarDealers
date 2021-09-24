9f468310dc30fd8a1271dccc006590cdc6d82f60
Mohamed Raseen <raseen.m@firstonesystems.com>
2021-09-24 16:43:59 +0530

    container styles

--- src/components/CarDealers.js
+++ src/components/CarDealers.js
@@ -7,29 +7,32 @@ import React from 'react';
 const CarDealers = ({ dealers }) => {
     return (
         <div className='dealers'>
-            <h4>Available Dealers</h4>
+            <div className='dealers-list'>
+                <h4>Available Dealers</h4>
 
-            <div className='card-details-container'>
-                {
-                    dealers && dealers.map((dealer, index) => {
-                        return (
-                            <Card key={index}>
-                                <CardImg top width="100%" src={dealer.image} alt="Card image cap" />
-                                <CardBody>
-                                    <CardTitle tag='h4'>{dealer.showRoomName}</CardTitle>
-                                    <CardSubtitle tag='h5'>{dealer.brandName}</CardSubtitle>
-                                    <CardText>
-                                        <div className='card-content'>
+                <div className='card-details-container'>
+                    {
+                        dealers && dealers.map((dealer, index) => {
+                            return (
+                                <Card key={index}>
+                                    <CardImg top width="100%" src={dealer.image} alt="Card image cap" />
+                                    <CardBody>
+                                        <CardTitle tag='h4'>{dealer.showRoomName}</CardTitle>
+                                        <CardSubtitle tag='h5'>{dealer.brandName}</CardSubtitle>
+                                        <CardText>
+                                            <div className='card-content'>
                                                 <div>Phone Number : {dealer.PhoneNumber}</div>
                                                 <div>Location : {dealer.location}</div>
-                                        </div>
-                                    </CardText>
-                                </CardBody>
-                            </Card>
-                        )
-                    })
-                }
+                                            </div>
+                                        </CardText>
+                                    </CardBody>
+                                </Card>
+                            )
+                        })
+                    }
+                </div>
             </div>
+
         </div>
     )
 }
